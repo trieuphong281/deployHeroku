e08b747735125d138547af073dfc1abbf78ee955
Nagi281 <n.h.quangtinh281@gmail.com>
2019-07-19 14:01:09 +0700

    delete some files which are no longer needed

--- timecheck.js
+++
@@ -1,8 +0,0 @@
-function isAfter() {
-    const requestTime = new Date();
-    // return ((requestTime.getHours() >= 13 && requestTime.getMinutes() >= 15)
-    //     || (requestTime.getHours() > 13));
-    return ((requestTime.getHours() === 13 ? requestTime.getMinutes() >= 9 : requestTime.getHours() > 13));
-}
-
-console.log(isAfter());
